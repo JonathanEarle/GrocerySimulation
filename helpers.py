5ba2448f7880efd419a8e528988eb10bbcc540c8
JonathanEarle <jonathan.john.enock.earle@gmail.com>
2017-04-13 12:51:56 -0400

    Cast #items to int

--- helpers.py
+++ helpers.py
@@ -26,7 +26,7 @@ def getData():
 				serviceStart.append(row[2])
 				servceEnd.append(row[3])
 				droptime.append(row[4])
-				items.append(row[5])
+				items.append(int(row[5]))
 				card.append(row[6])
 				waitTime.append(toSeconds(row[7]))
 				serviceTime.append(toSeconds(row[8]))
@@ -56,40 +56,6 @@ def getWaitTime(queue):
 def getServiceUtil(arrival,service):
 	return arrival/service
 
-def getLength(queue):
-    stat = 0
-    save = 0
-    arrival=queue[0]
-    s_start=queue[1]
-    y = 0
-    i = 0
-    count = 1 #start at 1 due to for loop starting from index 1
-    quesize = [count]
-    for i in range(1,len(arrival)):
-        skip = 0
-        while s_start[y] == 0: #skip to next y if drop out is 0
-            y+=1
-        if s_start[i]==0: #minus from queue if drop out
-            count-=1
-        else:
-            if arrival[i] > s_start[y]:
-                if(stat==0):
-                    save =i
-                stat=1
-                while s_start[y+skip+1]!=0 and s_start[y] > s_start[y+skip+1]: 
-                    skip+=1
-
-                count-=skip
-                if skip==0:
-                    y+=1
-                else:
-                    y+=skip
-            else:
-                count+=1
-        quesize.append(count)
-    return save,quesize
-
-
 def main():
 	getData()
 	for queue in Queues:
@@ -98,9 +64,7 @@ def main():
 		arr=getArrivalRate(Queues[queue])
 		ser=getServiceRate(Queues[queue])
 		wait=getWaitTime(Queues[queue])
-		length = getLength(Queues[queue])
 
-		print "Length of Queue " ,length
 		print "Arrival Rate " ,arr
 		print "Service Rate " ,ser
 		print "Wait Time " ,wait
