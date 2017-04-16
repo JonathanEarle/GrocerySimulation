f8d8a64beab63821a93d3d50d332529a9e23cd27
Kerschel <kerschels@hotmail.com>
2017-04-16 08:46:16 -0400

    Add files via upload

--- Modules/generateCustomer.py
+++ Modules/generateCustomer.py
@@ -5,11 +5,6 @@ from random import randrange
 import datetime 
 import helpers as hlp
 
-def random_time(start,ranges):
-   current = start
-   curr = current + datetime.timedelta(minutes=randrange(int(ranges)))
-   yield curr
-
 # Generates a customer with item card and enter time
 def genCustomer(queue):
 	arr=hlp.getArrivalRate(queue)
@@ -17,11 +12,7 @@ def genCustomer(queue):
 	startDate = datetime.datetime(2017, 9, 20,13,00)
 	card = hlp.bernolliCard(queue)
 	items = hlp.sampleItems(queue[4])
-	# Using the arrival time ratio it determines how many people a min so 60* arr gives the next interval for customer to enter
-	for time in random_time(startDate,arr*60):
-		enterTime = time.strftime("%H:%M:%S")
-	startDate = datetime.datetime(2017, 9, 20,13,int(time.strftime('%M')))
-	return {'items':items,'card':card,'arrival':enterTime}
+	return {'items':items,'card':card}
 
 
 def main():
