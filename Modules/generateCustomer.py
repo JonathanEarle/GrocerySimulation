513a892d4881e2d48705e75d4e6fb45472eeb873
Kerschel <kerschels@hotmail.com>
2017-04-16 08:55:01 -0400

    Add files via upload

--- Modules/generateCustomer.py
+++ Modules/generateCustomer.py
@@ -1,18 +1,13 @@
 #Holds all the functions to generate a customer
 
 #Create a customer dictionary {items,card,enter time} only num items and if they use a card are generated
-from random import randrange
-import datetime 
 import helpers as hlp
 
 # Generates a customer with item card and enter time
 def genCustomer(queue):
-	arr=hlp.getArrivalRate(queue)
-	amt = 30/float(arr)
-	startDate = datetime.datetime(2017, 9, 20,13,00)
 	card = hlp.bernolliCard(queue)
 	items = hlp.sampleItems(queue[4])
-	return {'items':items,'card':card}
+	return {'items':items,'card':card,'arrival':''}
 
 
 def main():
